b29d34a044d9019325dd9f7efbd5e66b326debe0
Carlos Carrillo <90294685+cjst3ch@users.noreply.github.com>
2022-06-03 15:53:06 -0500

    Added buttons for style changes

---
+++ cssMod.js
@@ -0,0 +1,11 @@
+function HTMLOnly() {
+    document.getElementById("styles").setAttribute("href", "");
+}
+
+function CSS1() {
+    document.getElementById("styles").setAttribute("href", "style.css");
+}
+
+function CSS2() {
+    document.getElementById("styles").setAttribute("href", "style2.css");
+}
